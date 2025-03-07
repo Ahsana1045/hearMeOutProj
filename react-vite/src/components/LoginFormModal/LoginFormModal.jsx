7547051ddeb0b931c68864e5e79106c39b45ba0a
Ali <ahsana1045@gmail.com>
2025-03-07 02:02:57 -0800

    seeded more characters. Login Demo added

--- react-vite/src/components/LoginFormModal/LoginFormModal.jsx
+++ react-vite/src/components/LoginFormModal/LoginFormModal.jsx
@@ -28,6 +28,22 @@ function LoginFormModal() {
     }
   };
 
+  // Function to log in as the demo user
+  const handleDemoLogin = async () => {
+    const demoUser = {
+      email: "demo@aa.io",
+      password: "password",
+    };
+
+    const serverResponse = await dispatch(thunkLogin(demoUser));
+
+    if (serverResponse) {
+      setErrors(serverResponse);
+    } else {
+      closeModal();
+    }
+  };
+
   return (
     <>
       <h1>Log In</h1>
@@ -54,8 +70,81 @@ function LoginFormModal() {
         {errors.password && <p>{errors.password}</p>}
         <button type="submit">Log In</button>
       </form>
+
+      {/* Demo User Login Button */}
+      <button onClick={handleDemoLogin} className="demo-login-button">
+        Log in as Demo User
+      </button>
     </>
   );
 }
 
 export default LoginFormModal;
+
+
+
+
+
+// WITHOUT DEMO LOG IN
+
+// import { useState } from "react";
+// import { thunkLogin } from "../../redux/session";
+// import { useDispatch } from "react-redux";
+// import { useModal } from "../../context/Modal";
+// import "./LoginForm.css";
+
+// function LoginFormModal() {
+//   const dispatch = useDispatch();
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [errors, setErrors] = useState({});
+//   const { closeModal } = useModal();
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+
+//     const serverResponse = await dispatch(
+//       thunkLogin({
+//         email,
+//         password,
+//       })
+//     );
+
+//     if (serverResponse) {
+//       setErrors(serverResponse);
+//     } else {
+//       closeModal();
+//     }
+//   };
+
+//   return (
+//     <>
+//       <h1>Log In</h1>
+//       <form onSubmit={handleSubmit}>
+//         <label>
+//           Email
+//           <input
+//             type="text"
+//             value={email}
+//             onChange={(e) => setEmail(e.target.value)}
+//             required
+//           />
+//         </label>
+//         {errors.email && <p>{errors.email}</p>}
+//         <label>
+//           Password
+//           <input
+//             type="password"
+//             value={password}
+//             onChange={(e) => setPassword(e.target.value)}
+//             required
+//           />
+//         </label>
+//         {errors.password && <p>{errors.password}</p>}
+//         <button type="submit">Log In</button>
+//       </form>
+//     </>
+//   );
+// }
+
+// export default LoginFormModal;
