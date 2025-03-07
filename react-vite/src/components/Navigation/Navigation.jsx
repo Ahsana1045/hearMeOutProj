48fd66752cf3ce4e4212fe85db58bfd57221a6aa
Ali <ahsana1045@gmail.com>
2025-03-07 00:22:02 -0800

    Navbar Completed

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -4,13 +4,38 @@ import "./Navigation.css";
 
 function Navigation() {
   return (
-    <ul>
-
-      <NavLink to="/">Hear Me Out?</NavLink>
-
-      <ProfileButton />
-    </ul>
+    <nav className="navbar">
+      <div className="left">
+        <NavLink to="/" className="nav-link">
+          <img src="/public/blackFire.png" alt=""  className="logo"/>
+        </NavLink>
+      </div>
+      <div className="right">
+        <ProfileButton />
+      </div>
+    </nav>
   );
 }
 
 export default Navigation;
+
+
+
+
+
+// import { NavLink } from "react-router-dom";
+// import ProfileButton from "./ProfileButton";
+// import "./Navigation.css";
+
+// function Navigation() {
+//   return (
+//     <ul>
+
+//       <NavLink to="/">Hear Me Out?</NavLink>
+
+//       <ProfileButton />
+//     </ul>
+//   );
+// }
+
+// export default Navigation;
